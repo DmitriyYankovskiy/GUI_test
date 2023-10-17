b6ce5f62da0ac236fc50bd77543c014e1dc07348
DmitriyYankovskiy <dmityank@gmail.com>
2023-10-17 17:14:33 +0300

    iio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Sandbox, Settings};
+use iced::widget::{Text, Column};
+
+fn main() -> iced::Result{
+    Model::run(Settings::default())
+}
+
+struct Model {
+    count: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Msg {
+    Incr,
+    Decr,
+}
+
+impl Sandbox for Model {
+    type Message = Msg;
+
+    fn new() -> Self {
+        Model { count: 0 }
+    }
+
+    fn title(&self) -> String{
+        String::from("Counter")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Msg::Incr => self.count += 1,
+            Msg::Decr => self.count -= 1,
+        }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        let label = Text::new(self.count.to_string());
+        Column::new().push(label).into()
+    }
 }
